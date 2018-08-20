7da39ff4f4642da75fdc72701e99abed683e78bd
EVAOTA <nervevawaka@ezweb.ne.jp>
2018-08-20 17:33:20 +0900

    added bootstrap and flask program

---
+++ FrameWork/Bootstrap4_Flask/Flask.py
@@ -0,0 +1,13 @@
+# index.html must put floder into templates
+# Accessing URL is http://IP_address:8080/ 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+	return render_template("./index.html")
+	
+if __name__ == "__main__":
+	app.debug = True
+	app.run(host="0.0.0.0",port=8080)
